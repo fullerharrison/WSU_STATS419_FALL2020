aeb9fdfa1de3a66a66c1ffd219644da7af96d2af
fullerharrison <harrison.fuller@wsu.edu>
2020-12-06 13:44:08 -0800

    final update

---
+++ libraries/final_libraries.R
@@ -0,0 +1,30 @@
+require(expss);         # NBA data
+require(tidyverse);     # NBA and Measurement data
+
+require(measurements);  # Measurement data
+require(stringr); 
+library(devtools);
+require(humanVerseWSU); # Basic Statistics and removing outliers of measurement data
+require(reshape2);      # melting df for visualization
+
+require(Hmisc);         # corr table
+
+require(REdaS);         # Clustering, KMO
+
+require(psych);         # sphericity
+
+require(openNLP);       # NLP
+require(NLP)
+require(tesseract);
+library(imdb);          # IMDB data
+require(magick);
+require(tripack);
+require(lsa);
+require(matrixStats)
+
+
+require(quanteda);require(quanteda.corpora);  #NLP text as multivariate
+require(quanteda.corpora);require(quanteda.textmodels);
+require(LIWCalike)
+
+require(DescTools); require(devtools);  # sourcing functions and etc
